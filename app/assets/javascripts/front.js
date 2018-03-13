195b28a94e8bbb1c0d3c09c275f7016334877126
Matthis Le Texier <matthis.le-texier@lunaweb.fr>
2018-03-13 15:16:22 +0100

    Reduce code scope to v1 projection

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -1,10 +1,9 @@
-//= resuire picturefill/picturefill.js
-//= require svg4everybody/svg4everybody.js
+// require picturefill/picturefill.js
+// require svg4everybody/svg4everybody.js
 //= require vendors/jquery.ba-throttle-debounce.min.js
 //= require_self
 //= require front/_logo.js
 //= require front/_footer.js
-// require front/_hero.js
 //= require front/_hero-rotate3d.js
 //= require front/_echo.js
 
@@ -13,4 +12,4 @@
 //     $html = $('html'),
 //     $body = $('body');
 
-svg4everybody();
+// svg4everybody();
