f3ea5563f81b7a3b38e3b0a7fa3166b28650b642
Matthis Le Texier <matthis.le-texier@lunaweb.fr>
2018-03-15 18:02:56 +0100

    Add vh improvement for mobiles
    Check vh offset on mobiles (browserbars height) and make it usable
    through a CSS variable.

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -1,6 +1,7 @@
 // require picturefill/picturefill.js
 // require svg4everybody/svg4everybody.js
 //= require vendors/jquery.ba-throttle-debounce.min.js
+//= require front/vh-check.js
 //= require_self
 //= require front/_logo.js
 //= require front/_footer.js
