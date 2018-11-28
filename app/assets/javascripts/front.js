9861384fd7cb71cfbbeb26b5c2009b789f815be2
Matthis Le Texier <matthis.le-texier@lunaweb.fr>
2018-11-28 14:18:17 +0100

    Optimize layout

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -1,11 +1,11 @@
 // require picturefill/picturefill.js
 // require svg4everybody/svg4everybody.js
 //= require vendors/jquery.ba-throttle-debounce.min.js
-//= require front/vh-check.js
+//= require front/vunit-check.js
 //= require_self
 //= require front/_cursor.js
 //= require front/_logo.js
-//= require front/_footer.js
+// require front/_footer.js
 //= require front/_hero-rotate3d.js
 //= require front/_echo.js
 //= require front/_tale.js
